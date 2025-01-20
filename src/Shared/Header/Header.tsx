ada66402660ef06d427e9028bd2425516af6ff64
zergy <xxdarkbluedevilxx@gmail.com>
2025-01-20 17:34:38 +0200

    Add Ptofile icon and route

--- src/Shared/Header/Header.tsx
+++ src/Shared/Header/Header.tsx
@@ -1,32 +1,49 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import styles from './Header.module.scss'
-import { faGithubAlt } from '@fortawesome/free-brands-svg-icons'
-import { faCodeBranch } from '@fortawesome/free-solid-svg-icons'
-import logoFlask from '../../images/logoFlask.png'
-import { Link } from 'react-router-dom'
-import { homepage } from '../../routes'
-import { Tooltip } from 'antd'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import styles from "./Header.module.scss";
+import { faGithubAlt } from "@fortawesome/free-brands-svg-icons";
+import { faCodeBranch, faUser } from "@fortawesome/free-solid-svg-icons";
+import logoFlask from "../../images/logoFlask.png";
+import { Link, useNavigate } from "react-router-dom";
+import { homepage, profile } from "../../routes";
+import { Tooltip } from "antd";
 
 const Header = () => {
-    return (
-        <div className={styles.container}>
-            <Link to={homepage} style={{ height: 26 }}>
-                <img src={logoFlask} alt='logo' height={26} className={styles.logo} />
-            </Link>
-            <div className={styles.linksContainer} >
-                <Tooltip title='Source code' placement='bottomRight' arrow={false}>
-                    <a href='https://github.com/zzergy/pixel-craft-studio' className={styles.link}>
-                        <FontAwesomeIcon icon={faCodeBranch} className={styles.small} />
-                    </a>
-                </Tooltip>
-                <Tooltip title='My GitHub Account 👩‍💻' placement='bottomRight' arrow={false}>
-                    <a href='https://github.com/zzergy' className={styles.link}>
-                        <FontAwesomeIcon icon={faGithubAlt} className={styles.medium} />
-                    </a>
-                </Tooltip>
-            </div>
-        </div>
-    )
-}
+  const navigate = useNavigate();
 
-export default Header
+  return (
+    <div className={styles.container}>
+      <Link to={homepage} style={{ height: 26 }}>
+        <img src={logoFlask} alt="logo" height={26} className={styles.logo} />
+      </Link>
+      <div className={styles.iconContainer}>
+        <Tooltip title="Source code" placement="bottomRight" arrow={false}>
+          <a
+            href="https://github.com/zzergy/pixel-craft-studio"
+            className={styles.icon}
+          >
+            <FontAwesomeIcon icon={faCodeBranch} />
+          </a>
+        </Tooltip>
+        <Tooltip
+          title="My GitHub Account 👩‍💻"
+          placement="bottomRight"
+          arrow={false}
+        >
+          <a href="https://github.com/zzergy" className={styles.icon}>
+            <FontAwesomeIcon icon={faGithubAlt} className={styles.medium} />
+          </a>
+        </Tooltip>
+
+        <Tooltip title="Profile" placement="bottomRight" arrow={false}>
+          <FontAwesomeIcon
+            icon={faUser}
+            className={styles.icon}
+            onClick={() => navigate(profile)}
+          />
+        </Tooltip>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
--- src/routes.ts
+++ src/routes.ts
@@ -2,3 +2,4 @@ export const homepage = "/";
 export const canvas = "/canvas";
 export const signup = "/signup";
 export const signin = "/signin";
+export const profile = "/profile";
